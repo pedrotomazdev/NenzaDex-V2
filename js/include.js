916948f2eab8a26e5042b07906c6771e99a1dbe2
Pedro Tomaz <pedrotomaz420@gmail.com>
2025-07-12 11:58:22 -0300

    Melhorias no cabeçalho, ícone de áudio adicionado na página do pokémon

--- js/include.js
+++ js/include.js
@@ -1,16 +1,16 @@
 import { globalFunctions } from "./dom.js";
 
 async function includeHTML(selector, file) {
-  const el = document.querySelector(selector);
-  if (!el) return;
-  try {
-    const res = await fetch(file);
-    if (!res.ok) throw new Error(`Erro ao carregar ${file}`);
-    el.innerHTML = await res.text();
-  } catch (err) {
-    console.error(err);
-    el.innerHTML = `<p>Erro ao carregar conteúdo.</p>`;
-  }
+    const el = document.querySelector(selector);
+    if (!el) return;
+    try {
+        const res = await fetch(file);
+        if (!res.ok) throw new Error(`Erro ao carregar ${file}`);
+        el.innerHTML = await res.text();
+    } catch (err) {
+        console.error(err);
+        el.innerHTML = `<p>Erro ao carregar conteúdo.</p>`;
+    }
 }
 
 // inclui header e footer automaticamente
@@ -18,42 +18,51 @@ includeHTML('#include-header', '/partials/header.html');
 includeHTML('#include-footer', '/partials/footer.html');
 
 const waitForToggleButton = setInterval(() => {
-  const toggleBtn = document.getElementById('toggle-dark');
-  if (toggleBtn) {
-    // Aplica o tema salvo
-    if (localStorage.getItem('theme') === 'dark') {
-      document.body.classList.add('dark-mode');
-      document.documentElement.classList.add('dark-mode');
+    const toggleBtn = document.getElementById('toggle-dark');
+    if (toggleBtn) {
+        // Aplica o tema salvo
+        if (localStorage.getItem('theme') === 'dark') {
+            document.body.classList.add('dark-mode');
+            document.documentElement.classList.add('dark-mode');
+        }
+
+        toggleBtn.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+            document.documentElement.classList.toggle('dark-mode');
+
+            if (document.body.classList.contains('dark-mode')) {
+                localStorage.setItem('theme', 'dark');
+            } else {
+                localStorage.setItem('theme', 'light');
+            }
+        });
+
+        globalFunctions.initSearch();
+        globalFunctions.initRandom();
+
+        clearInterval(waitForToggleButton);
     }
-
-    toggleBtn.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-      document.documentElement.classList.toggle('dark-mode');
-
-      if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-      } else {
-        localStorage.setItem('theme', 'light');
-      }
-    });
-
-    globalFunctions.initSearch();
-    globalFunctions.initRandom();
-
-    clearInterval(waitForToggleButton);
-  }
 }, 100);
 
 // Aplica o tema salvo quando a página carrega
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' }); // ajusta o valor conforme o quanto quer descer
-  }, 500);
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    document.body.classList.add('dark-mode');
-    document.documentElement.classList.add('dark-mode');
+    setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // ajusta o valor conforme o quanto quer descer
+    }, 500);
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-mode');
+        document.documentElement.classList.add('dark-mode');
 
-  }
+    }
+    window.addEventListener('scroll', () => {
+        const header = document.querySelector('header');
+        if (window.scrollY > 200) {
+            header.classList.add('minimized');
+        } else {
+            header.classList.remove('minimized');
+        }
+    });
+
 });
 
